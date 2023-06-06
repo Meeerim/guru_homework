8e1c3b7eb03a421d491a2311e3df522dbcb30a4a
Meerim Sabytkyzy <skmeerim1999@gmail.com>
2023-06-06 11:58:28 +0400

    fix after code review

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -2,36 +2,36 @@
 Параметризуйте фикстуру несколькими вариантами размеров окна
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
+
 import pytest
 
-from selene import browser
+from selene import browser, be
 from selenium import webdriver
 
 
-@pytest.fixture(params=[(1920, 1280), (1600, 1024), (1242, 2688), (828, 1792)])
+@pytest.fixture(params=[(1920, 1280), (1242, 2688), (360, 640), (414, 896)],
+                ids=['desktop', 'desktop', 'mobile', 'mobile'])
 def browser_setup(request):
     chrome_options = webdriver.ChromeOptions()
     browser.config.driver_options = chrome_options
     browser.config.window_height = request.param[0]
     browser.config.window_width = request.param[1]
-
+    if 'desktop' in request.node.name and 'mobile' in request.node.callspec.id:
+        pytest.skip('Пропускаем тесты т.к соотношение сторон не подходит')
+    elif 'mobile' in request.node.name and 'desktop' in request.node.callspec.id:
+        pytest.skip('Пропускаем тесты т.к соотношение сторон не подходит')
     yield
     browser.quit()
 
 
 def test_github_desktop(browser_setup):
-    if browser.config.window_height < 1242:
-        pytest.skip('Пропускаем мобильные тесты т.к соотношение сторон не подходит')
-    else:
-        browser.open("https://github.com/")
-        browser.element('a.HeaderMenu-link--sign-in').click()
+    browser.open("https://github.com/")
+    browser.config.timeout = 2.0
+    browser.element('a.HeaderMenu-link--sign-in').should(be.visible).click()
 
 
 def test_github_mobile(browser_setup):
-    if browser.config.window_height > 1279:
-        pytest.skip("Пропускаем декстопные тесты т.к соотношение сторон не подходит")
-    else:
         browser.open('https://github.com')
         browser.config.timeout = 2.0
         browser.element('.flex-column [aria-label="Toggle navigation"]').click()
-        browser.element('a.HeaderMenu-link--sign-in').click()
+        browser.element('a.HeaderMenu-link--sign-in').should(be.visible).click()
